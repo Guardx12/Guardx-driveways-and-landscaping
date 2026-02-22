fe8b548bafd4ea0744ee5423d84f58e59d6e0b13
biglabox121-eng <biglabox121@gmail.com>
2026-02-22 18:16:51 +0000

    up

---
+++ components/Icons.js
@@ -0,0 +1,72 @@
+export function IconArrow(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M5 12h12" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+      <path d="M13 6l6 6-6 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+export function IconPhone(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M22 16.92v3a2 2 0 0 1-2.18 2A19.8 19.8 0 0 1 3 5.18 2 2 0 0 1 5.11 3h3a2 2 0 0 1 2 1.72c.13.98.35 1.94.66 2.86a2 2 0 0 1-.45 2.11L9.09 10.91a16 16 0 0 0 4 4l1.22-1.23a2 2 0 0 1 2.11-.45c.92.31 1.88.53 2.86.66A2 2 0 0 1 22 16.92Z" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+export function IconMail(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M4 6h16v12H4V6Z" stroke="currentColor" strokeWidth="1.6"/>
+      <path d="m4 7 8 6 8-6" stroke="currentColor" strokeWidth="1.6" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+export function IconMap(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M21 10c0 6-9 12-9 12S3 16 3 10a9 9 0 1 1 18 0Z" stroke="currentColor" strokeWidth="1.6"/>
+      <path d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" stroke="currentColor" strokeWidth="1.6"/>
+    </svg>
+  );
+}
+export function IconStar(props){
+  return (
+    <svg viewBox="0 0 24 24" width="14" height="14" fill="currentColor" {...props}>
+      <path d="M12 17.27 18.18 21 16.54 13.97 22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27Z"/>
+    </svg>
+  );
+}
+export function IconGoogle(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M21.6 12.27c0-.73-.07-1.43-.2-2.1H12v3.98h5.37c-.23 1.24-.94 2.29-2 3.01v2.5h3.23c1.9-1.75 3-4.33 3-7.39Z" fill="currentColor" opacity="0.92"/>
+      <path d="M12 22c2.7 0 4.97-.9 6.63-2.44l-3.23-2.5c-.9.6-2.04.95-3.4.95-2.6 0-4.8-1.76-5.58-4.12H3.08v2.58A10 10 0 0 0 12 22Z" fill="currentColor" opacity="0.70"/>
+      <path d="M6.42 13.89A6 6 0 0 1 6.1 12c0-.66.12-1.3.32-1.89V7.53H3.08A10 10 0 0 0 2 12c0 1.61.39 3.13 1.08 4.47l3.34-2.58Z" fill="currentColor" opacity="0.52"/>
+      <path d="M12 5.98c1.47 0 2.79.51 3.83 1.5l2.87-2.87C16.97 2.9 14.7 2 12 2A10 10 0 0 0 3.08 7.53l3.34 2.58C7.2 7.74 9.4 5.98 12 5.98Z" fill="currentColor" opacity="0.85"/>
+    </svg>
+  );
+}
+export function IconFacebook(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" {...props}>
+      <path d="M22 12a10 10 0 1 0-11.56 9.88v-7H8.9V12h1.54V9.8c0-1.52.9-2.36 2.28-2.36.66 0 1.35.12 1.35.12v1.49h-.76c-.75 0-.98.47-.98.95V12h1.67l-.27 2.88h-1.4v7A10 10 0 0 0 22 12Z"/>
+    </svg>
+  );
+}
+export function IconInstagram(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5Z" stroke="currentColor" strokeWidth="1.6"/>
+      <path d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" stroke="currentColor" strokeWidth="1.6"/>
+      <path d="M17.5 6.5h.01" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round"/>
+    </svg>
+  );
+}
+export function IconLeaf(props){
+  return (
+    <svg viewBox="0 0 24 24" width="18" height="18" fill="none" {...props}>
+      <path d="M20 4s-7.5 1-11 4.5S4 20 4 20s7.5-1 11-4.5S20 4 20 4Z" stroke="currentColor" strokeWidth="1.8" strokeLinejoin="round"/>
+      <path d="M9 15c.8-2.6 3.2-5 7-7" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round"/>
+    </svg>
+  );
+}
