3d112e6168bd51d3b0347f046626ee5c2cc8e329
biglabox121-eng <biglabox121@gmail.com>
2026-02-22 18:32:10 +0000

    up

--- components/Icons.js
+++ components/Icons.js
@@ -36,29 +36,37 @@ export function IconStar(props){
     </svg>
   );
 }
-export function IconGoogle(props){
+export function IconGoogleMark(props){
   return (
-    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
-      <path d="M21.6 12.27c0-.73-.07-1.43-.2-2.1H12v3.98h5.37c-.23 1.24-.94 2.29-2 3.01v2.5h3.23c1.9-1.75 3-4.33 3-7.39Z" fill="currentColor" opacity="0.92"/>
-      <path d="M12 22c2.7 0 4.97-.9 6.63-2.44l-3.23-2.5c-.9.6-2.04.95-3.4.95-2.6 0-4.8-1.76-5.58-4.12H3.08v2.58A10 10 0 0 0 12 22Z" fill="currentColor" opacity="0.70"/>
-      <path d="M6.42 13.89A6 6 0 0 1 6.1 12c0-.66.12-1.3.32-1.89V7.53H3.08A10 10 0 0 0 2 12c0 1.61.39 3.13 1.08 4.47l3.34-2.58Z" fill="currentColor" opacity="0.52"/>
-      <path d="M12 5.98c1.47 0 2.79.51 3.83 1.5l2.87-2.87C16.97 2.9 14.7 2 12 2A10 10 0 0 0 3.08 7.53l3.34 2.58C7.2 7.74 9.4 5.98 12 5.98Z" fill="currentColor" opacity="0.85"/>
+    <svg viewBox="0 0 256 262" width="16" height="16" {...props}>
+      <path fill="#4285F4" d="M255.68 133.45c0-11.1-.99-22.2-3.1-32.9H130.5v62.3h69.8c-3.01 16.2-12.2 29.9-25.9 39.1v25.8h41.9c24.6-22.7 39.4-56.1 39.4-94.3z"/>
+      <path fill="#34A853" d="M130.5 261.1c35.1 0 64.6-11.6 86.2-31.5l-41.9-25.8c-11.7 7.9-26.7 12.4-44.3 12.4-33.8 0-62.4-22.8-72.7-53.4H14.6v26.5c21.4 42.5 65.3 71.8 115.9 71.8z"/>
+      <path fill="#FBBC05" d="M57.8 162.8c-5.4-16.2-5.4-33.8 0-50V86.3H14.6c-18.3 36.5-18.3 79.7 0 116.2l43.2-39.7z"/>
+      <path fill="#EA4335" d="M130.5 45.9c19.1-.3 37.5 6.9 51.6 20.1l38.3-38.3C193.7 10.2 163.3-0.2 130.5 0 79.9 0 36 29.3 14.6 71.8l43.2 39.7c10.3-30.6 38.9-53.4 72.7-53.4z"/>
     </svg>
   );
 }
-export function IconFacebook(props){
+export function IconFacebookMark(props){
   return (
-    <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" {...props}>
-      <path d="M22 12a10 10 0 1 0-11.56 9.88v-7H8.9V12h1.54V9.8c0-1.52.9-2.36 2.28-2.36.66 0 1.35.12 1.35.12v1.49h-.76c-.75 0-.98.47-.98.95V12h1.67l-.27 2.88h-1.4v7A10 10 0 0 0 22 12Z"/>
+    <svg viewBox="0 0 24 24" width="16" height="16" {...props}>
+      <path fill="#1877F2" d="M24 12.07C24 5.41 18.63 0 12 0S0 5.41 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.5h3.05V9.43c0-3.03 1.8-4.7 4.56-4.7 1.32 0 2.7.24 2.7.24v2.98h-1.52c-1.5 0-1.96.94-1.96 1.9v2.29h3.33l-.53 3.5h-2.8V24C19.61 23.1 24 18.1 24 12.07z"/>
     </svg>
   );
 }
-export function IconInstagram(props){
+export function IconInstagramMark(props){
   return (
-    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
-      <path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5Z" stroke="currentColor" strokeWidth="1.6"/>
-      <path d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" stroke="currentColor" strokeWidth="1.6"/>
-      <path d="M17.5 6.5h.01" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round"/>
+    <svg viewBox="0 0 24 24" width="16" height="16" {...props}>
+      <defs>
+        <linearGradient id="ig" x1="0" x2="1" y1="1" y2="0">
+          <stop offset="0" stopColor="#F58529"/>
+          <stop offset="0.35" stopColor="#DD2A7B"/>
+          <stop offset="0.7" stopColor="#8134AF"/>
+          <stop offset="1" stopColor="#515BD4"/>
+        </linearGradient>
+      </defs>
+      <path fill="url(#ig)" d="M7 0h10a7 7 0 0 1 7 7v10a7 7 0 0 1-7 7H7a7 7 0 0 1-7-7V7a7 7 0 0 1 7-7z"/>
+      <path fill="#fff" d="M12 6.3A5.7 5.7 0 1 0 12 17.7 5.7 5.7 0 0 0 12 6.3zm0 9.4A3.7 3.7 0 1 1 12 8a3.7 3.7 0 0 1 0 7.7z"/>
+      <path fill="#fff" d="M18.2 5.6a1.3 1.3 0 1 0 0 2.6 1.3 1.3 0 0 0 0-2.6z"/>
     </svg>
   );
 }
@@ -67,6 +75,14 @@ export function IconLeaf(props){
     <svg viewBox="0 0 24 24" width="18" height="18" fill="none" {...props}>
       <path d="M20 4s-7.5 1-11 4.5S4 20 4 20s7.5-1 11-4.5S20 4 20 4Z" stroke="currentColor" strokeWidth="1.8" strokeLinejoin="round"/>
       <path d="M9 15c.8-2.6 3.2-5 7-7" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round"/>
+    </svg>
+  );
+}
+export function IconSpark(props){
+  return (
+    <svg viewBox="0 0 24 24" width="16" height="16" fill="none" {...props}>
+      <path d="M12 2l1.4 5.1L18 9l-4.6 1.9L12 16l-1.4-5.1L6 9l4.6-1.9L12 2Z" fill="currentColor" opacity="0.9"/>
+      <path d="M4 14l.8 2.6L7.4 18 4.8 18.9 4 21.5 3.2 18.9.6 18l2.6-1.4L4 14Z" fill="currentColor" opacity="0.7"/>
     </svg>
   );
 }
